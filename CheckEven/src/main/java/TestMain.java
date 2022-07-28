eeb2abc1492146e17db149df2bb03965e8137897
AshishNikam123 <nikamsoham46@gmail.com>
2022-07-28 15:31:34 +0530

    Added java Even application

---
+++ CheckEven/src/main/java/TestMain.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+@Test
+public void testInputIsEven()
+{
+assertTrue(Main.checkIfInputIsAnEvenNumber(23)); //Assertion
+}
+}
